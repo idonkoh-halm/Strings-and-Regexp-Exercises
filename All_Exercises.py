e9061216dd4c72b871abe83364913959d4fdb206
Isaac Donkoh-Halm <isaac.donkoh-halm@innovationcharter.org>
2016-03-18 12:35:07 -0400

    Start of Assignment:
    A file was made for each assignment; additionally a file
    “Isaac_Assignment.py” contains all of the files in one code

---
+++ All_Exercises.py
@@ -0,0 +1,28 @@
+def formatting_strings():
+    print "This is where the formatting strings code will go after you just said the term: Formatting Strings"
+
+def join():
+    pass
+def regexp_find_all():
+    pass
+def regexp_for_word_games():
+    pass
+def regexp_for_word_games_2():
+    pass
+def regexp_for_word_games_3():
+    pass
+def regexp_test_for_a_phone_number():
+    pass
+
+Grading=raw_input("I want to grade Isaac's code on:")
+if Grading=="Formatting Strings":
+    formatting_strings()
+elif Grading =="formatting strings":
+    formatting_strings()
+elif Grading=="Formatting strings":
+    formatting_strings()
+elif Grading=="formatting Strings":
+    formatting_strings()
+else:
+    print "It's not gonna work. You can try the individual code if this is too buggy."
+
